927e0f9be43ced5dfaf9d8781195fecbaa73a4d4
Gooya Bamedi <gooya.bamedi@gmail.com>
2026-04-06 13:35:42 +0330

    new supabase config

--- src/supabase.js
+++ src/supabase.js
@@ -1,9 +1,12 @@
-// supabase.js
+// src/supabase.js
 import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-export const supabaseUrl = 'http://192.168.86.231:8000'; 
+// YOUR NEW IP
+export const laptopIp = '192.168.86.231'; 
+
+export const supabaseUrl = `http://${laptopIp}:8000`;
 
 // THE DEFAULT SUPABASE LOCAL ANON KEY
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE';
